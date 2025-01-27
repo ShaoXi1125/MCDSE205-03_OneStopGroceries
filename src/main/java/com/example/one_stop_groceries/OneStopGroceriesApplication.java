c2336aa1aab9b8e5d7d419efa0273b6a06fbd16f
ShaoXi <shaoxi9733@gmail.com>
2025-01-27 13:57:25 +0800

    init commit

---
+++ src/main/java/com/example/one_stop_groceries/OneStopGroceriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.one_stop_groceries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneStopGroceriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneStopGroceriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/one_stop_groceries/OneStopGroceriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.one_stop_groceries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneStopGroceriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
